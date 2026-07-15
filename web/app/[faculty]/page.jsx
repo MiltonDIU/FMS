1cabedc27e7c8c6c67010260c1366fc175426996
Md Milton <web3@daffodilvarsity.edu.bd>
2026-07-15 10:56:40 +0600

    Delete web directory
    No need this directory

--- web/app/[faculty]/page.jsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import { use } from "react";
-import Home from "../page";
-
-export default function FacultyPage({ params }) {
-  const resolvedParams = use(params);
-  return <Home params={resolvedParams} />;
-}
