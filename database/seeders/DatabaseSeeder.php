0e036938e918312d31a0f49ca31ec28592972662
MiltonDIU <web3@daffodilvarsity.edu.bd>
2026-07-18 16:35:27 +0600

    Update pdf file and manage inside system settings which information are download or not

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -45,6 +45,7 @@ class DatabaseSeeder extends Seeder
             ApprovalPermissionsSeeder::class,
             NotificationRoutingSeeder::class,
             RoleSevenPermissionSeeder::class,
+            BrandingSettingsSeeder::class,
 //            AdministrativeRoleUserSeeder::class,
         ]);
     }
